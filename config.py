86220c4783fe47dcfdd75a559c6cbe163f8ef016
phalla009 <hphalla3@gmail.com>
2025-08-11 16:36:33 +0700

    Initial commit

---
+++ config.py
@@ -0,0 +1,17 @@
+
+# Telegram Bot Settings
+BOT_TOKEN = "8282363808:AAGT0lAZyrs8Q5gTUDY2hgpfBDc_4KfepIk"
+CHAT_ID = "@khchelrien2024"
+
+# Currency rate USD → KHR
+USD_TO_KHR = 4100
+
+# Flask-Mail config - example for Gmail SMTP
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = 'hphalla3@gmail.com'
+MAIL_PASSWORD = 'iflq jnct ngwy ucqg'
+MAIL_DEFAULT_SENDER = 'hphalla3@gmail.com'
+
+
