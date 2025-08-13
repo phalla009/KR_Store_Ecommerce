f48c4cb906eee1fc32921db1369fd5b12280b17d
phalla009 <hphalla3@gmail.com>
2025-08-13 13:24:18 +0700

    Edited file

--- app.py
+++ app.py
@@ -1804,65 +1804,6 @@ products =  [
         "https://cdn.dummyjson.com/product-images/groceries/kiwi/1.webp"
       ],
       "thumbnail": "https://cdn.dummyjson.com/product-images/groceries/kiwi/thumbnail.webp"
-    },{
-      "id": 101,
-      "title": "Apple AirPods Max Silver",
-      "description": "The Apple AirPods Max in Silver are premium over-ear headphones with high-fidelity audio, adaptive EQ, and active noise cancellation. Experience immersive sound in style.",
-      "category": "mobile-accessories",
-      "price": 549.99,
-      "discountPercentage": 13.67,
-      "rating": 3.47,
-      "stock": 59,
-      "tags": [
-        "electronics",
-        "over-ear headphones"
-      ],
-      "brand": "Apple",
-      "sku": "MOB-APP-APP-101",
-      "weight": 2,
-      "dimensions": {
-        "width": 24.88,
-        "height": 14.9,
-        "depth": 27.54
-      },
-      "warrantyInformation": "No warranty",
-      "shippingInformation": "Ships in 2 weeks",
-      "availabilityStatus": "In Stock",
-      "reviews": [
-        {
-          "rating": 5,
-          "comment": "Excellent quality!",
-          "date": "2025-04-30T09:41:02.053Z",
-          "reviewerName": "Henry Adams",
-          "reviewerEmail": "henry.adams@x.dummyjson.com"
-        },
-        {
-          "rating": 4,
-          "comment": "Very happy with my purchase!",
-          "date": "2025-04-30T09:41:02.053Z",
-          "reviewerName": "Elijah Cruz",
-          "reviewerEmail": "elijah.cruz@x.dummyjson.com"
-        },
-        {
-          "rating": 4,
-          "comment": "Would buy again!",
-          "date": "2025-04-30T09:41:02.053Z",
-          "reviewerName": "William Lopez",
-          "reviewerEmail": "william.lopez@x.dummyjson.com"
-        }
-      ],
-      "returnPolicy": "No return policy",
-      "minimumOrderQuantity": 1,
-      "meta": {
-        "createdAt": "2025-04-30T09:41:02.053Z",
-        "updatedAt": "2025-04-30T09:41:02.053Z",
-        "barcode": "4062176053732",
-        "qrCode": "https://cdn.dummyjson.com/public/qr-code.png"
-      },
-      "images": [
-        "https://cdn.dummyjson.com/product-images/mobile-accessories/apple-airpods-max-silver/1.webp"
-      ],
-      "thumbnail": "https://cdn.dummyjson.com/product-images/mobile-accessories/apple-airpods-max-silver/thumbnail.webp"
     }
   ]
 
