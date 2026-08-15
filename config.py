93ec6d00e0e812f5a62902a5def804d9d0544b2f
phalla009 <hphalla3@gmail.com>
2026-08-15 16:11:22 +0700

    Include config.py for production

--- config.py
+++ config.py
@@ -1,17 +1,24 @@
+import os
+from flask import Flask
 
-# Telegram Bot Settings
-BOT_TOKEN = "8282363808:AAGT0lAZyrs8Q5gTUDY2hgpfBDc_4KfepIk"
-CHAT_ID = "@khchelrien2024"
+app = Flask(__name__)
+app.secret_key = os.environ.get('SECRET_KEY', os.urandom(24))
 
-# Currency rate USD → KHR
+# Telegram & Currency Settings
+BOT_TOKEN = os.environ.get(
+    'BOT_TOKEN', '8282363808:AAGT0lAZyrs8Q5gTUDY2hgpfBDc_4KfepIk'
+)
+CHAT_ID = os.environ.get('CHAT_ID', '@khchelrien2024')
 USD_TO_KHR = 4100
 
-# Flask-Mail config - example for Gmail SMTP
-MAIL_SERVER = 'smtp.gmail.com'
-MAIL_PORT = 587
-MAIL_USE_TLS = True
-MAIL_USERNAME = 'hphalla@gmail.com'
-MAIL_PASSWORD = 'iflq jnct ngwy ucqg'
-MAIL_DEFAULT_SENDER = 'hphalla3@gmail.com'
-
-
+# Flask-Mail Configuration
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME', 'hphalla@gmail.com')
+app.config['MAIL_PASSWORD'] = os.environ.get(
+    'MAIL_PASSWORD', 'iflq jnct ngwy ucqg'
+)
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get(
+    'MAIL_DEFAULT_SENDER', 'hphalla3@gmail.com'
+)
