9151228e4583050f97945d467ea2fdc53d7fd10f
phalla009 <hphalla3@gmail.com>
2026-08-15 16:24:09 +0700

    Include config.py for production

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.config.from_object('config')
 app.secret_key = os.urandom(24)
 mail = Mail(app)
 # Create a Blueprint with your prefix
-bp = Blueprint('krecomstore', __name__, url_prefix='/krecomstore')
+bp = Blueprint('main', __name__, url_prefix='/krecomstore')
 products =[
     {
       "id": 1,
